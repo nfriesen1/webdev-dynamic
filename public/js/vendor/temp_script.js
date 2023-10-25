b30a88b557772747cafad237ad237580728f9feb
nfriesen1 <frie1379@stthomas.edu>
2023-10-25 14:34:58 -0500

    changes to home page and temp page

--- public/js/vendor/temp_script.js
+++ public/js/vendor/temp_script.js
@@ -2,11 +2,21 @@ document.getElementById('tempForm').addEventListener('submit', function (event) 
     event.preventDefault(); // Prevent the default form submission behavior
 
     // Get user input
-    const temp1 = document.getElementById('temp1').value;
-    const temp2 = document.getElementById('temp2').value;
+    const temp1 =parseInt(document.getElementById('temp1').value)
+    const temp2 = parseInt(document.getElementById('temp2').value)
 
-    // Construct the URL with path parameters
-    const url = `/temp/${temp1}/${temp2}`;
+    console.log(temp1)
+    console.log(temp2)
+
+    if(!Number.isInteger(temp1) || !Number.isInteger(temp2)) {
+        console.log(temp1)
+        console.log(temp2)
+        alert("Temperature entered must be a whole number")
+    } else if (temp1 > temp2) {
+        alert("Min temperature must be greater than or equal too the max temperature")
+    
+    } else {
+        const url = `/temp/${temp1}/${temp2}`;
 
     // Perform a GET request using fetch
     fetch(url)
@@ -19,4 +29,4 @@ document.getElementById('tempForm').addEventListener('submit', function (event) 
             // Handle any errors (e.g., display an error message)
             console.error("Error:", error);
         });
-});
+    }})
